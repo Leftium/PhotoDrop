7dcef78722c6d583fa61707547b8761b264f7ab4
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-05-29 03:31:47 -0500

    Add link to Dropbox request.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
     import '../app.scss';
+
+    import { PUBLIC_DROPBOX_REQUEST_LINK } from '$env/static/public';
 </script>
 
 <main class="container">
-    <h1>Welcome to SvelteKit</h1>
-    <p>
-        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-    </p>
+    <a href={PUBLIC_DROPBOX_REQUEST_LINK}>
+        <h1>Share your photos from this event!</h1>
+    </a>
+
+    <hr />
+
+    <h1>View photos from this event!</h1>
+    <p>[TODO]</p>
 </main>
