40900880f5a61153b64e3641989c004fbb8ad8c4
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-05-29 04:28:12 -0500

    Add Dropbox embedder.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,67 @@
 <script lang="ts">
     import '../app.scss';
 
-    import { PUBLIC_DROPBOX_REQUEST_LINK } from '$env/static/public';
+    import { onMount } from 'svelte';
+
+    import {
+        PUBLIC_DROPBOX_APP_KEY,
+        PUBLIC_DROPBOX_REQUEST_LINK,
+        PUBLIC_DROPBOX_SHARED_LINK,
+    } from '$env/static/public';
+
+    let embedderDiv: HTMLElement;
+
+    onMount(() => {
+        const options = {
+            link: PUBLIC_DROPBOX_SHARED_LINK,
+            file: {
+                zoom: 'best',
+            },
+            folder: {
+                view: 'grid',
+                headerSize: 'small',
+            },
+        };
+        (window as any).Dropbox.embed(options, embedderDiv);
+    });
 </script>
 
+<svelte:head>
+    <script
+        type="text/javascript"
+        src="https://www.dropbox.com/static/api/2/dropins.js"
+        id="dropboxjs"
+        data-app-key={PUBLIC_DROPBOX_APP_KEY}
+    ></script>
+</svelte:head>
+
 <main class="container">
-    <a href={PUBLIC_DROPBOX_REQUEST_LINK}>
-        <h1>Share your photos from this event!</h1>
-    </a>
+    <h1>
+        <a href={PUBLIC_DROPBOX_REQUEST_LINK}>
+            Share your photos from this event!
+        </a>
+    </h1>
 
     <hr />
 
     <h1>View photos from this event!</h1>
-    <p>[TODO]</p>
+
+    <div class="embedder" bind:this={embedderDiv} />
 </main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+
+        height: 100%;
+    }
+
+    h1 {
+        margin: auto;
+    }
+
+    .embedder {
+        flex-grow: 1;
+    }
+</style>
